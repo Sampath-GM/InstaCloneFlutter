872928a9dfd8144fb76092e6124daf8f5b218f40
Sampath-GM <sampathgm0341@gmail.com>
2023-10-20 16:05:34 +0530

    renamed app name

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import "package:flutter/material.dart";
 import "package:google_fonts/google_fonts.dart";
 import 'package:sampat/pages/Home.dart';
-import 'package:sampat/login.dart';
+import "package:sampat/pages/login.dart";
+
 import "package:sampat/utils/route.dart";
   
 void main(){
